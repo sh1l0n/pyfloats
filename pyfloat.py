cf435d9f19fbb4b72c06859ccef3b78d0bbf17cf
chancla15 <jesusmanresa1@gmail.com>
2020-12-20 01:15:42 +0100

    add headers

--- pyfloat.py
+++ pyfloat.py
@@ -1,7 +1,20 @@
-#
-# Copyright author 2020
-# Created by __sh0l1n@
-#
+#!/usr/bin/env python
+"""
+    Copyright (C) 2020 __sh0l1n.
+   
+    Contributed by __sh0l1n <https://github.com/sh1l0n>, 2020.
+    PyFloat is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+    PyFloat Library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public
+    License along with the PyFloat Library; if not, see
+    <http://www.gnu.org/licenses/>. 
+"""
 
 class PyFloat:
     def __init__(self, n="0"):
